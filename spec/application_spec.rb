10ddaa4a6454f8f54a77c2ce4fb2083214ba31bb
patrickclery <patrick.clery@gmail.com>
2019-09-09 15:25:25 -0700

    :sparkles: Introducing new features. Added basic RSpec setup

---
+++ spec/application_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe 'Shell application' do
+
+  include_context 'default_context'
+
+  it 'is executable from the command-line' do
+    expect(File.executable?("./app.rb")).to be true
+  end
+
+  # Use this test if you use any switches
+  it 'can accept an output file as an argument' do
+    pending
+    fail
+    # exit_status = system("./application.rb -o #{tmp_file.path}")
+    # expect(exit_status).to be true
+    #
+    # csv = File.open(tmp_file.path)
+    # expect(csv.size).to be_nonzero
+  end
+
+end
---
+++ spec/examples/default_examples.rb
@@ -0,0 +1,3 @@
+RSpec.shared_examples 'default_examples' do
+  # Default examples go here
+end
