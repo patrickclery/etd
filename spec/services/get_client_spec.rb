bcc599def01fbc4d30c2a3effbea5dfa710ed475
patrickclery <patrick.clery@gmail.com>
2019-09-09 18:35:50 -0700

    :recycle: Refactoring code.

--- spec/services/get_client_spec.rb
+++ spec/services/get_client_spec.rb
@@ -14,14 +14,13 @@ RSpec.describe App::GetClient do
                  token:           nil,
                  sandbox:         true)
   end
+
   context 'with Evernote execution disabled' do
     before(:each) do
-      client = double("EvernoteOAuth::Client")
-      dbl = double(EvernoteOAuth::Client.new)
-      allow(client).to receive(:new).with({ consumer_key:    'barbara',
-                                            consumer_secret: '0123456789abcdef',
-                                            token:           nil,
-                                            sandbox:         true }).and_return(dbl)
+      allow(EvernoteOAuth::Client).to receive(:new).with({ consumer_key:    'barbara',
+                                                           consumer_secret: '0123456789abcdef',
+                                                           token:           nil,
+                                                           sandbox:         true }).and_return(EvernoteOAuth::Client.new)
     end
 
     it 'returns an instance of an Evernote API Client' do
@@ -30,7 +29,7 @@ RSpec.describe App::GetClient do
                                token:           nil,
                                sandbox:         true) }
 
-      expect(func.call).to be_a_kind_of(EvernoteOAuth::Client)
+      expect(func.call).to be_a(EvernoteOAuth::Client)
     end
   end
 end
